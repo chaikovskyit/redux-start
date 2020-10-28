2200f425cb3628d98567b790032f2bf2277dbf32
chaikovskyit <chaikovskyit@gmail.com>
2020-10-28 17:14:13 +0200

    create counter

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React, {Component} from 'react'
 import './App.css';
 
-class App extends Comment {
+class App extends Component {
+
   state = {
     counter: 0
   }
 
+  updateCounter(value){
+    this.setState({
+      counter: this.state.counter + value
+    })
+  }
+  
   render() {
     return(
       <div className='App'>
-
+        <h1>Counter <strong>{this.state.counter}</strong></h1>
+        <hr/>
+        <div className="Actions">
+          <button onClick={() => this.updateCounter(1)}>+</button>
+          <button onClick={() => this.updateCounter(-1)}>-</button>
+        </div>
       </div>
     )
   }
