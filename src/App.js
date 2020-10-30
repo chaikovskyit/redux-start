1c3d1a11bffa8fd8e945dfa4daa51919862d3ca3
chaikovskyit <chaikovskyit@gmail.com>
2020-10-30 15:40:01 +0200

    add decomposition to my reducer

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 // функція яка дозволяє зєднати компонент reactu з store redux
 import {connect} from 'react-redux'
 import './App.css';
-
+import Counter from './Counter'
 class App extends Component {
   // updateCounter(value){
   //   this.setState({
@@ -20,7 +20,14 @@ class App extends Component {
           <button onClick={this.props.onAdd}>+</button>
           {/* <button onClick={() => this.updateCounter(-1)}>+</button> */}
           <button onClick={this.props.onSub}>-</button>
+
+          <button onClick={() => this.props.onAddNumber(1)}>1</button>
+          <button onClick={() => this.props.onAddNumber(2)}>2</button>
+          <button onClick={() => this.props.onAddNumber(3)}>3</button>
+          <button onClick={() => this.props.onAddNumber(4)}>4</button>
         </div>
+        <hr/>
+        <Counter />
       </div>
     )
   }
@@ -29,15 +36,16 @@ class App extends Component {
 function mapStateToProps(state){
   // повертаємо новий JS обєкт трансформований, для того щоб його юзати не як "state", а як "props". "state.counter" ми беремо з "rootReducer.js"
   return {
-    counter: state.counter
+    counter: state.counter1.counter
   }
 }
 
-// повертає нам оюєкт
+// повертає нам об'єкт з екшенами
 function mapDispatchToProps(dispatch) {
   return {
-    onAdd: () => dispatch({type: 'ADD', value: 42}),
-    onSub: () => dispatch({type: 'SUB'})
+    onAdd: () => dispatch({type: 'ADD', value: Math.random()}),
+    onSub: () => dispatch({type: 'SUB', value: Math.random()}),
+    onAddNumber: number => dispatch({type: 'ADD_NUMBER', payload: number})
   }
 }
 
--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,21 +1,40 @@
-// задаємо початкове значення state
-const initialState = {
-  counter: 0
-}
-// Оскільки це наш "Reducer" то ми передаємо в нього "state" та "action"
-export default function rootReducer(state = initialState, action) {
-  // Логіка для виконання наших екшенів
-  switch(action.type) {
-    case 'ADD':
-      return {
-        counter: state.counter + action.value
-      }
-    case 'SUB':
-      return {
-        counter: state.counter - 1
-      }
-  }
-  
-  // "Reducer" завжди повертає змінений "state"
-  return state
-} 
+// // задаємо початкове значення state
+// const initialState = {
+//   counter: 0
+// }
+// // Оскільки це наш "Reducer" то ми передаємо в нього "state" та "action"
+// export default function rootReducer(state = initialState, action) {
+//   // Логіка для виконання наших екшенів
+//   switch(action.type) {
+//     case 'ADD':
+//       return {
+//         counter: state.counter + 1
+//       }
+//     case 'SUB':
+//       return {
+//         counter: state.counter - 1
+//       }
+//     case 'ADD_NUMBER':
+//       return {
+//         counter: state.counter + action.payload
+//       }
+//     default:
+//       return state    
+//   }
+
+//   // "Reducer" завжди повертає змінений "state"
+//   return state
+// } 
+
+
+
+
+// функція яка обєднує редюсери
+import {combineReducers} from 'redux'
+
+import counter1 from './reducers/counter1'
+import counter2 from './reducers/counter2'
+
+export default combineReducers({
+  counter1, counter2
+})
